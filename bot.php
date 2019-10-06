0fb23d46c5ceffc0242ebd6aece519934fce9337
dimon <vampirua@gmail.com>
2019-10-06 13:48:30 +0300

    ff

--- bot.php
+++ bot.php
@@ -2,7 +2,24 @@
 require_once "vendor/autoload.php";
 $token = "982221383:AAEgNznDDyQdYXeC_6eoO33jZ3mXDE_YM88";
 $bot = new \TelegramBot\Api\Client($token);
-// команда для start
+
+
+$json = file_get_contents('https://api.privatbank.ua/p24api/exchange_rates?json&date=06.10.2019');
+$obj = json_decode($json);
+
+foreach ($json as $item) {
+
+    if ($item->currency == 'USD') {
+        $sale = $item->saleRate;
+        $buy = $item->purchaseRate;
+        $bot->command('USD', function ($message, $sale, $buy) use ($bot) {
+
+            $answer = " Sale :$sale , Buy : $buy";
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+    }
+}
+
 $bot->command('start', function ($message) use ($bot) {
     $answer = 'Добро пожаловать!';
     $bot->sendMessage($message->getChat()->getId(), $answer);
