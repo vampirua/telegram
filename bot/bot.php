082057931b4a4f45db0f25bc52741dba1b1b45b5
dimon <vampirua@gmail.com>
2019-10-06 12:06:04 +0300

    ff

--- bot/bot.php
+++ bot/bot.php
@@ -1,1 +1,19 @@
-<p>hi</p>
+<?php
+require_once "vendor/autoload.php";
+$token = "AAEgNznDDyQdYXeC_6eoO33jZ3mXDE_YM88";
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
+?>
