662cab62629e912dcfa5e6f2ce2bcd50d41e96b6
dimon <vampirua@gmail.com>
2019-10-11 13:35:53 +0300

    ff

--- bot.php
+++ bot.php
@@ -13,8 +13,7 @@ $bot->command('USD', function ($message) use ($bot, $obj, $current_date) {
         var_dump($item);
         if ($item->ccy == 'USD') {
             $sale = $item->buy . 'UAH';
-            $buy = $item->sale . 'UAH'.
-            $current_date;
+            $buy = $item->sale . 'UAH'. "$current_date";
 
         }
     }
