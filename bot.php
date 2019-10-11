7fe3ca04972d022edb789058e2952dd6feedf9e5
dimon <vampirua@gmail.com>
2019-10-11 14:08:03 +0300

    ff

--- bot.php
+++ bot.php
@@ -8,6 +8,17 @@ date_default_timezone_set('Ukraine/Kiev'); // CDT
 
 $current_date = date('d/m/Y');
 
+
+$keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+    [
+        [
+            ['text' => 'link', 'url' => 'https://core.telegram.org']
+        ]
+    ]
+);
+
+
+
 $bot->command('USD', function ($message) use ($bot, $obj, $current_date) {
     foreach ($obj as $item) {
         var_dump($item);
@@ -58,32 +69,4 @@ $bot->command('BTC', function ($message) use ($bot, $obj, $current_date) {
 });
 
 $bot->run();
-
-$api = 'https://api.telegram.org/bot' . $token;
-$output = json_decode(file_get_contents('php://input'), TRUE);
-$chat_id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
-$callback_query = $output['callback_query'];
-$data = $callback_query['data'];
-$message_id = ['callback_query']['message']['message_id'];
-$chat_id_in = $callback_query['message']['chat']['id'];
-switch ($message) {
-    case '/test':
-        $inline_button1 = array("text" => "Google url", "url" => "http://google.com");
-        $inline_button2 = array("text" => "work plz", "callback_data" => '/plz');
-        $inline_keyboard = [[$inline_button1, $inline_button2]];
-        $keyboard = array("inline_keyboard" => $inline_keyboard);
-        $replyMarkup = json_encode($keyboard);
-        sendMessage($chat_id, "ok", $replyMarkup);
-        break;
-}
-switch ($data) {
-    case '/plz':
-        sendMessage($chat_id_in, "plz");
-        break;
-}
-function sendMessage($chat_id, $message, $replyMarkup)
-{
-    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup=' . $replyMarkup);
-}
 ?>
