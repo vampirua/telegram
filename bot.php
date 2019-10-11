121080cd24034107b017ec80d97760d413d83045
dimon <vampirua@gmail.com>
2019-10-11 13:19:45 +0300

    ff

--- bot.php
+++ bot.php
@@ -5,30 +5,40 @@ $bot = new \TelegramBot\Api\Client($token);
 $json = file_get_contents('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
 $obj = json_decode($json);
 
-$bot->command('USD', function ($message) use ($bot,$obj) {
+$bot->command('USD', function ($message) use ($bot, $obj) {
     foreach ($obj as $item) {
         var_dump($item);
-            if ($item->ccy == 'USD') {
-
-                var_dump($sale = $item->buy);
-                $buy = $item->sale;
-            }
+        if ($item->ccy == 'USD') {
+            $sale = $item->buy.'UAH';
+            $buy = $item->sale.'UAH';
+        }
     }
     $answer = " Sale :$sale , Buy : $buy";
     $bot->sendMessage($message->getChat()->getId(), $answer);
 
 });
 
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
+$bot->command('EUR', function ($message) use ($bot, $obj) {
+    foreach ($obj as $item) {
+        if ($item->ccy == 'EUR') {
+            $sale = $item->buy.'UAH';
+            $buy = $item->sale.'UAH';
+        }
+    }
+    $answer = " Sale :$sale , Buy : $buy";
     $bot->sendMessage($message->getChat()->getId(), $answer);
-});
 
-// команда для помощи
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - вывод справки';
+});
+$bot->command('RUR', function ($message) use ($bot, $obj) {
+    foreach ($obj as $item) {
+        if ($item->ccy == 'RUR') {
+            $sale = $item->buy.'UAH';
+            $buy = $item->sale.'UAH';
+        }
+    }
+    $answer = " Sale :$sale , Buy : $buy";
     $bot->sendMessage($message->getChat()->getId(), $answer);
+
 });
 
 $bot->run();
