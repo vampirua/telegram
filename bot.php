db00590cedda0d94cb0da580ff100580213d1ed7
dimon <vampirua@gmail.com>
2019-10-11 14:02:03 +0300

    ff

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ $bot->command('USD', function ($message) use ($bot, $obj, $current_date) {
         var_dump($item);
         if ($item->ccy == 'USD') {
             $sale = $item->buy . 'UAH';
-            $buy = $item->sale . 'UAH' .'   ' ." $current_date";
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
 
         }
     }
@@ -26,7 +26,7 @@ $bot->command('EUR', function ($message) use ($bot, $obj, $current_date) {
     foreach ($obj as $item) {
         if ($item->ccy == 'EUR') {
             $sale = $item->buy . 'UAH';
-            $buy = $item->sale . 'UAH' .'   ' ." $current_date";
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
 
         }
     }
@@ -38,7 +38,7 @@ $bot->command('RUR', function ($message) use ($bot, $obj, $current_date) {
     foreach ($obj as $item) {
         if ($item->ccy == 'RUR') {
             $sale = $item->buy . 'UAH';
-            $buy = $item->sale . 'UAH' .'   ' ." $current_date";
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
         }
     }
     $answer = " Sale :$sale , Buy : $buy";
@@ -49,13 +49,42 @@ $bot->command('BTC', function ($message) use ($bot, $obj, $current_date) {
     foreach ($obj as $item) {
         if ($item->ccy == 'BTC') {
             $sale = $item->buy . 'UAH';
-            $buy = $item->sale . 'UAH' .'   ' ." $current_date";
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
         }
     }
     $answer = " Sale :$sale , Buy : $buy";
     $bot->sendMessage($message->getChat()->getId(), $answer);
 
 });
+
+
+$api = 'https://api.telegram.org/bot' . $token;
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$message = $output['message']['text'];
+$callback_query = $output['callback_query'];
+$data = $callback_query['data'];
+$message_id = ['callback_query']['message']['message_id'];
+$chat_id_in = $callback_query['message']['chat']['id'];
+switch ($message) {
+    case '/test':
+        $inline_button1 = array("text" => "Google url", "url" => "http://google.com");
+        $inline_button2 = array("text" => "work plz", "callback_data" => '/plz');
+        $inline_keyboard = [[$inline_button1, $inline_button2]];
+        $keyboard = array("inline_keyboard" => $inline_keyboard);
+        $replyMarkup = json_encode($keyboard);
+        sendMessage($chat_id, "ok", $replyMarkup);
+        break;
+}
+switch ($data) {
+    case '/plz':
+        sendMessage($chat_id_in, "plz");
+        break;
+}
+function sendMessage($chat_id, $message, $replyMarkup)
+{
+    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup=' . $replyMarkup);
+}
 
 
 $bot->run();
