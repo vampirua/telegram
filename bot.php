dec64398c11fee6798099842a54af3fb9fd833a7
dimon <vampirua@gmail.com>
2019-10-11 14:19:39 +0300

    ff

--- bot.php
+++ bot.php
@@ -9,9 +9,64 @@ date_default_timezone_set('Ukraine/Kiev'); // CDT
 $current_date = date('d/m/Y');
 
 
-$bot = new \TelegramBot\Api\BotApi('982221383:AAEgNznDDyQdYXeC_6eoO33jZ3mXDE_YM88');
 
-$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array(array("one", "two", "three")), true); // true for one-time keyboard
 
-$bot->sendMessage($chatId, $messageText, null, false, null, $keyboard);
+
+$bot->command('ff', function ($message) use ($bot, $obj, $current_date) {
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array(array("one", "two", "three")), true); // true for one-time keyboard
+    $messageText = ' ff';
+    $bot->sendMessage($message->getChat()->getId(), $messageText, null, false, null, $keyboard);
+});
+$bot->command('USD', function ($message) use ($bot, $obj, $current_date) {
+    foreach ($obj as $item) {
+        var_dump($item);
+        if ($item->ccy == 'USD') {
+            $sale = $item->buy . 'UAH';
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
+
+        }
+    }
+    $answer = " Sale :$sale , Buy : $buy";
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+
+});
+
+$bot->command('EUR', function ($message) use ($bot, $obj, $current_date) {
+    foreach ($obj as $item) {
+        if ($item->ccy == 'EUR') {
+            $sale = $item->buy . 'UAH';
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
+
+        }
+    }
+    $answer = " Sale :$sale , Buy : $buy";
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+
+});
+$bot->command('RUR', function ($message) use ($bot, $obj, $current_date) {
+    foreach ($obj as $item) {
+        if ($item->ccy == 'RUR') {
+            $sale = $item->buy . 'UAH';
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
+        }
+    }
+    $answer = " Sale :$sale , Buy : $buy";
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+
+});
+$bot->command('BTC', function ($message) use ($bot, $obj, $current_date) {
+    foreach ($obj as $item) {
+        if ($item->ccy == 'BTC') {
+            $sale = $item->buy . 'UAH';
+            $buy = $item->sale . 'UAH' . '   ' . " $current_date";
+        }
+    }
+    $answer = " Sale :$sale , Buy : $buy";
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+
+});
+
+$bot->run();
+
+
 ?>
